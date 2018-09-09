a87d182204ae429807acc5ae193b46398483c374
pankaja-shree <pankajashree@gmail.com>
2018-09-09 14:42:57 +0530

    format the dialog box

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,13 @@
           <v-dialog v-model="dialog" scrollable>
           <v-btn slot="activator" round color="primary"> About Vrittaratnakara</v-btn>
           <v-card>
-            <v-card-title>Vrittaratnakara</v-card-title>
+            <v-card-title><span class="headline">Vrittaratnakara</span></v-card-title>
             <v-divider></v-divider>
             <v-card-text>Vritta Ratnakara of Kedara Bhatta (14th Century CE) is one of
 the most popular texts on Sanskrit prosody. Though there are many books on Sanskrit prosody by eminent authors like Kalidasa, Kshemendra, etc, Vritta Ratankara continues to be an essential text for Sanskrit students. A speciality of this work is that the definition and illustration of a meter is given in one and the same verse. The verse defining a particular metre is composed in that particular meter itself. This is very helpful for a student of Sanskrit prosody. Another specialty of this work is that it is very brief – there are merely 136 verses. The author has covered all the prominent metres of Sanskrit literature in these verses.</v-card-text>
             <v-divider></v-divider>
             <v-card-actions>
+              <v-spacer></v-spacer>
               <v-btn color="blue" flat @click.native="dialog = false">Close</v-btn>
             </v-card-actions>
           </v-card>
@@ -33,3 +34,15 @@ the most popular texts on Sanskrit prosody. Though there are many books on Sansk
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
+
+
