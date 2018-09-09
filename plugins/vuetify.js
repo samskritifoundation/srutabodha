5632e70346dbda9fa8acf761183ef67875bb6f08
pankaja-shree <pankajashree@gmail.com>
2018-09-09 14:43:18 +0530

    provide custom theme colours

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#B71C1C',
+    secondary: '#e57373',
+    accent: '#9c27b0',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#2196f3',
+    success: '#4caf50'
+  }
+})
