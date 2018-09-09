9e15f9294cdf64d7e7e3658d798880adf2366849
pankaja-shree <pankajashree@gmail.com>
2018-09-09 16:47:10 +0530

    add sf banner

---
+++ pages/contact.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <img src="../assets/img/samskriti_foundation_header.jpg" alt="Vuetify.js">
+      <p>Address:</p>
+    </v-flex>
+  </v-layout>
+</template>
