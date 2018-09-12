8f67a2da8c875902067a18c152193f46f027af33
pankaja-shree <pankajashree@gmail.com>
2018-09-12 11:52:07 +0530

    create new component

---
+++ pages/aboutsf.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-content>
+    <v-card color="transparent" flat>
+      <div class="text-xs-center">
+    <sflogo title="संस्कृति-प्रतिष्ठानम्" subtitle1="Samskriti Foundation" subtitle2="An organisation engaged in the preservation, revival and propagation of the Indian Knowledge Systems and Cultural and Scientific Heritage of India" />
+    </div>
+    </v-card>
+    <v-card color="transparent" flat>
+      <v-card-content class="subheading font-weight-bold text-xs-center">
+      Samskriti Foundation, Mysore is a Public Trust engaged in the activity of preservation and propagation of Cultural and Scientific Heritage of India. Over the last ten years of its existence, it has brought out several publications in the form of books, Audio CDs and Interactive Multimedia Computer CDs on various aspects of Indian cultural heritage, Manuscriptology and so on. Even as the world is seeing uncontrolled deterioration in all spheres of human life, the Foundation has embarked on a journey to once again re-establish the moral and spiritual values in the world, by its innovative and pioneering programmes that are aimed at enabling the world at large to live a life in harmony with nature and uphold the path of righteousness.
+Samskriti is an earnest endeavor to improve the quality of life as the quality of life is at its best only when man learns to live with nature in harmony.
+<br><br><p>Visit <a href="https://www.samskrti.org">our site</a> to know more.</p>
+    </v-card-content>
+    </v-card>
+    
+  </v-content>
+</template>
+
+<script>
+import sflogo from '../components/SFLogo.vue'
+export default {
+  components: {
+    sflogo
+  }
+}
+</script>
