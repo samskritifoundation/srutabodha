bcabf78cd493769791ac9224a036d7c0a505ff76
pankaja-shree <pankajashree@gmail.com>
2018-09-14 17:31:22 +0530

    create lessons page

---
+++ pages/lessons.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-layout column justify-center align-center class="papyrus">
+    <navleft />
+    <navright />
+  </v-layout>
+</template>
+
+<script>
+import navleft from '../components/lessonsnav.vue'
+import navright from '../components/rightnav.vue'
+
+export default {
+  data: () => ({
+    valid: false,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => v.length <= 30 || 'Name must be less than 30 characters'
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid'
+    ]
+  }),
+  components: {
+    navleft,
+    navright
+  },
+  methods: {
+    submit () {
+      if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
+        /*
+        axios.post('/api/submit', {
+          name: this.name,
+          email: this.email,
+          select: this.select,
+          checkbox: this.checkbox
+        })
+        */
+        alert('hello')
+      }
+    }
+  }
+}
+</script>
