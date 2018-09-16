78ed462f442947da735cee165db70570678e2916
pankaja-shree <pankajashree@gmail.com>
2018-09-16 16:26:19 +0530

    insert v-card

--- pages/lessons/_id.vue
+++ pages/lessons/_id.vue
@@ -1,11 +1,43 @@
 <template>
-<v-app>
-        lesson number
-</v-app>
+<v-container>
+<v-card color="secondary" dark class="papyrus">
+    <div class = "bordered">
+    <v-card-title primary-title>
+        <h3 class="papyrus myheader">{{lesson.title}}</h3>
+        </v-card-title>
+        <v-card-text>
+            <div class="devanagari">{{lesson.definition_sans}}</div>
+            <div>{{lesson.definition_eng}}</div>
+        </v-card-text>
+    </div>
+  </v-card>
+</v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      lesson: {
+        title: 'Introduction',
+        definition_sans: 'lorem ipsum',
+        definition_eng: 'lorem ipsum'
+      }
+    }
+  },
   layout: 'lessons'
 }
 </script>
+
+<style scoped>
+.bordered {
+      border: 10px solid transparent;
+-webkit-border-image: url('~/assets/img/border.png') 30 round; /* Safari 3.1-5 */
+    -o-border-image: url('~/assets/img/border.png') 30 round; /* Opera 11-12.1 */
+    border-image: url('~/assets/img/border.png') 30 round;
+    border-radius: 15px;
+    margin:0;
+    padding: 2%;
+}
+
+</style>
--- pages/lessons/index.vue
+++ pages/lessons/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-layout column justify-center align-center class="papyrus">
-    Hello
-  </v-layout>
+  <v-card dark color="secondary" class="bordered papyrus">
+    <v-card-title>Hello</v-card-title>
+  </v-card>
 </template>
 
 <script>
@@ -9,3 +9,15 @@ export default {
   layout: 'lessons'
 }
 </script>
+
+<style scoped>
+.bordered {
+      border: 10px solid transparent;
+-webkit-border-image: url('~/assets/img/border.png') 30 round; /* Safari 3.1-5 */
+    -o-border-image: url('~/assets/img/border.png') 30 round; /* Opera 11-12.1 */
+    border-image: url('~/assets/img/border.png') 30 round;
+    border-radius: 15px;
+    margin:0;
+    padding: 2%;
+}
+</style>
