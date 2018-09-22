2eb4dfffb6fd1f50b240b48324aa1887ff8a4998
pankaja-shree <pankajashree@gmail.com>
2018-09-22 17:10:39 +0530

    create exercises page

---
+++ pages/exercises.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="mt-0 pt-0">
+  <v-card color="accent lighten-4" class="bordered papyrus mt-0 pt-0">
+    <v-card-title primary-title>
+      <h3 class="papyrus myheader">Vrittaratnakara Exercises</h3>
+      </v-card-title>
+      Under construction
+  </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'lessons'
+}
+</script>
+
+<style scoped>
+.bordered {
+      border: 10px solid transparent;
+-webkit-border-image: url('~/assets/img/border.png') 30 round; /* Safari 3.1-5 */
+    -o-border-image: url('~/assets/img/border.png') 30 round; /* Opera 11-12.1 */
+    border-image: url('~/assets/img/border.png') 30 round;
+    border-radius: 15px;
+    margin:0;
+    padding: 2%;
+}
+</style>
