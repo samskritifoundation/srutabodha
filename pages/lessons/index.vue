0e2b966e7906a9f43e21bfd9f0ef376f245f5c84
pankaja-shree <pankajashree@gmail.com>
2018-09-22 11:19:31 +0530

    update content on lessons homepage

--- pages/lessons/index.vue
+++ pages/lessons/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <v-card dark color="secondary" class="bordered papyrus">
-    <v-card-title>Hello</v-card-title>
+  <v-container class="mt-0 pt-0">
+  <v-card color="accent lighten-4" class="bordered papyrus mt-0 pt-0">
+    <v-card-title primary-title>
+      <h3 class="papyrus myheader">Learn Vrittaratnakara</h3>
+      </v-card-title>
+      <v-btn to="/lessons/1" large color="secondary" class="text-capitalize font-weight-bold" round> Start with introduction to the field of Sanskrit Prosody</v-btn>
   </v-card>
+  </v-container>
 </template>
 
 <script>
