639e03067a0043b4f3b2ca552ece3bab45be84b5
pankaja-shree <pankajashree@gmail.com>
2018-11-10 16:24:26 +0530

    remove unwanted files

--- middleware/getLesson.js
+++
@@ -1,8 +0,0 @@
-/*
-var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb://root-user:ramanuja108@cluster0-shard-00-00-8wiwi.mongodb.net:27017,cluster0-shard-00-01-8wiwi.mongodb.net:27017,cluster0-shard-00-02-8wiwi.mongodb.net:27017/chandas?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-MongoClient.connect(uri, function(err, db) {
-   db.close();
-});
-*/
