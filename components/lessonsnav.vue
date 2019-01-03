ff4ae94963acaa30b2489694d077422f17c7a200
pankaja-shree <pankajashree@gmail.com>
2019-01-03 16:03:09 +0530

    add search bar

--- components/lessonsnav.vue
+++ components/lessonsnav.vue
@@ -46,6 +46,14 @@
         </v-list-tile>
       </v-list>
 
+      <v-text-field
+        v-model="searchlist"
+        append-icon="search"
+        label="Search"
+        single-line
+        class = "px-3"
+      ></v-text-field>
+
       <v-list class="pt-0" dense>
         <v-divider light></v-divider>
 
@@ -121,8 +129,9 @@
   export default {
     data () {
       return {
+        searchlist: '',
         drawer: null,
-        items: [
+        titles: [
           {
             title: 'Introduction to Shrutabodha',
             sl_num: '1',
@@ -384,6 +393,17 @@
         ],
         mini: false,
         right: null
+      }
+    },
+    computed: {
+      items () {
+        if (this.searchlist) {
+          return this.titles.filter(item => {
+            return item.title.includes(this.searchlist)
+          })
+        } else {
+          return this.titles
+        }
       }
     }
   }
